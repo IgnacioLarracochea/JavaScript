39769ac9136aac1a6ad706a382e020b103931a3d
unknown <ignacio_rato@hotmail.com>
2022-05-21 18:33:55 -0300

    proyecto ignacio Larracochea

--- js/Index.js
+++ js/Index.js
@@ -1,33 +1,62 @@
+/*
+//Escribir nombre de Paises en mayuscula
 let nombreUsuario = prompt("ingrese su nombre y apellido");
+let array = [
+  { id: "Argentina", IVA: 0.21 },
+  { id: "Brasil", IVA: 0.17 },
+  { id: "Chile", IVA: 0.19 },
+  { id: "Colombia", IVA: 0.16 },
+];
 
-if (nombreUsuario) {
-    
-let mensaje = "hola ";
 
-alert(mensaje + nombreUsuario);
-
-let numeroDocumento = prompt("ingrese su numero de documento");
-
-alert("usted califica para la solicitud de prestamo");
-
-alert("tenga en cuenta que el IVA es del 21%")
-
-let montoElegido = prompt("Ingrese monto deseado a solicitar");
-
-console.log(typeof montoElegido);
-
-let precioParse = parseFloat(montoElegido)
-
-let valorFinal = precioParse * 1.21;
-
-console.log(typeof valorFinal);
-
-alert("El monto a pagar mas el iva es: " + valorFinal)
-
+//nombre de usuario diferente a vacio
+if (!nombreUsuario == " ") {
+  alert("Bienvenido: " + nombreUsuario);
+  let numeroDocumento = prompt("ingrese su numero de documento");
+  let Edad = prompt("ingrese su edad");
+  //si documento es diferente a vacio y es mayor de 18 aÃ±os
+  if (!numeroDocumento == " " && Edad >= 18) {
+    alert("usted califica para la solicitud de prestamo");
+    let pais = prompt("Ingrese su pais de residencia");
+    //ingresa un pais y si es diferente a vacio y si no cumple con algun nombre del array
+    for (i = 0; i <= array.length; i++) {
+      if (pais == array[i].id) {
+        let IVA = array[i].IVA;
+        let montoElegido = prompt("Ingrese monto deseado a solicitar");
+        let precioParse = parseFloat(montoElegido);
+        let valorFinal = precioParse * IVA;
+        alert("El monto a pagar es: " + valorFinal + " y el IVA es: " + IVA);
+      } else if (!pais == array[i].id) {
+        alert(
+          "recarge la pagina y elija uno de los siguientes paises (Argentina,Brasil,Chile,Colombia)"
+        );
+      }
+    }
+  } else {
+    alert("Usted no es mayor de edad y no es posible pedir un prestamo");
+  }
 } else {
-console.log("No ha ingresado su nombre completo");
-
-
-
+  alert("No a ingresado su nombre, por favor recarge la pagina");
 }
 
+*/
+
+ const datos = document.getElementById("envio");
+ const envioForm = (event) => { 
+
+ event.preventDefault()
+ const name = document.getElementById("name").value 
+ const dni = document.getElementById("dni").value 
+ const edad = document.getElementById("edad").value 
+ const pais = document.getElementById("pais").value 
+ const prestamo = document.getElementById("prestamo").value
+
+ console.log(name)
+ console.log(dni)
+ console.log(edad)
+ console.log(pais)
+ console.log(prestamo)
+
+} 
+
+datos.addEventListener("submit", envioForm)
